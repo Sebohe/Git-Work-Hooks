b3b5d12a09577b41b8e589dcfea556d566ccd917
SebastianB <sebastian.b@toptier.com>
2016-06-03 09:26:22 -0700

    updating some self extracting scripts

--- self_extract.sh
+++ self_extract.sh
@@ -2,6 +2,7 @@
 #
 #Just execute this file to copy the GitTools repo contents into their corresponding directions
 files=$(ls Hooks)
-for x in $files; do 
+for x in $files; do
+	echo "self_extract"
 	cp Hooks/$x ../hooks/$x
 done
--- updateAll.sh
+++ updateAll.sh
@@ -2,8 +2,11 @@
 file=$(cat local_repos_list)
 IFS=$(echo -en "\n\b")
 for x in $file; do
+	unset $(git rev-parse --local-env-vars)
+	cd $x
+	pwd
+	./self_extract.sh
 
-	echo $x
 
 
 done

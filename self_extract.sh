1c8c46b1482af1c553325a6c6d18d08f03022bd8
SebastianB <sebastian.b@toptier.com>
2016-06-04 17:04:06 -0700

    working self update and self extract with local file list

--- self_extract.sh
+++ self_extract.sh
@@ -3,6 +3,6 @@
 #Just execute this file to copy the GitTools repo contents into their corresponding directions
 files=$(ls Hooks)
 for x in $files; do
-	echo "self_extract"
+	echo "copying... "$x
 	cp Hooks/$x ../hooks/$x
 done
--- updateAll.sh
+++ updateAll.sh
@@ -5,8 +5,7 @@ for x in $file; do
 	unset $(git rev-parse --local-env-vars)
 	cd $x
 	pwd
+	git remote update
+	git merge origin/master
 	./self_extract.sh
-
-
-
 done

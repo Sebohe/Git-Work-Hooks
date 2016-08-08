4d61217b403888ad9c04989f2bf522421c0af843
SebastianB <sebastian.b@toptier.com>
2016-08-08 15:18:28 -0700

    Updated updateAll intro text.

--- updateAll.sh
+++ updateAll.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#Updates all of the local repos for the GitHooks software
+#
+#Once the local repos are update it self extracts the contents of the repo to the apropiate directory of git.
+#
+#If you want to add a new local repository path add the path to local_repos_list
+#
 file=$(cat local_repos_list)
 IFS=$(echo -en "\n\b")
 for x in $file; do
